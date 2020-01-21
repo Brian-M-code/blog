85bf7e607af08aca700ca5704f3cc536bbfe3025
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-21 18:37:27 +0300

    deploying to heroku

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,7 +10,7 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    quote=requests.get_quote()
+    quote= requests.get_quote()
     return render_template('home.html', posts=posts, quote=quote)
 
 
