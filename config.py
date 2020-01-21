0f7ccca1ba3378ccf8425b8e468d39626db2225a
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-21 11:08:18 +0300

    added a test config sub-class

--- config.py
+++ config.py
@@ -10,14 +10,15 @@ class Config:
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    # MAIL_USE_SSL = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 
     QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+
 class ProdConfig(Config):
     '''
     Production  configuration class
@@ -25,16 +26,28 @@ class ProdConfig(Config):
     
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mutumas:Mutuma1234@localhost/blog'
+
+    
 class DevConfig(Config):
     '''
     Development  configuration class
     '''
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mutumas:Mutuma1234@localhost/blog'
+    
     DEBUG= True
     ENV = 'development'
     
 config_options = {
     'production' : ProdConfig,
-    'development' : DevConfig
+    'development' : DevConfig,
+    'test': TestConfig
 }
