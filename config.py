8ec60a8c6ca8c8cebf5fd38b58217489241ea0ae
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-21 16:16:32 +0300

    modified html files

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     '''
     debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mutumas:Mutuma1234@localhost/blog'
     
 
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME='mutuma.brian@yahoo.com'
 export MAIL_PASSWORD='Tush2105'
 export SECRET_KEY='brian'
+export WTF_CSRF_SECRET_KEY='a csrf secret key'
 
 python manage.py server
