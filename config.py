02e05482a083695f17360edb14d5afa7ae8d6d6f
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-21 08:52:11 +0300

    added config file

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+class Config:
+    '''
+    General configuration class
+    '''
+    debug = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mutumas:Mutuma1234@localhost/blog'
+    
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    # MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+
+    QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+class ProdConfig(Config):
+    '''
+    Production  configuration class
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    Development  configuration class
+    '''
+    DEBUG = True
+config_options = {
+    'production' : ProdConfig,
+    'development' : DevConfig
+}
