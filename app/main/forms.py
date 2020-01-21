b071501fae96a03862a91f221ca60421ce4c6299
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-21 12:07:23 +0300

    added class postform amd update profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,10 @@ class PostForm(FlaskForm):
     title = StringField("Blog title:", validators=[Required()])
     post = TextAreaField("Type Away:", validators=[Required()])
     submit = SubmitField("Post")
+    
+class UpdateProfile(FlaskForm):
+    first_name = StringField("First name")
+    last_name = StringField("Last Name")
+    bio = TextAreaField("Bio")
+    email = StringField("Email")
+    submit = SubmitField("Update")
