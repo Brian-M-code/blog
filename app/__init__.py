3e148d4c1073b367ce1d9bc72ac3e64619a83512
Brian-M-Code <mutuma.brian@yahoo.com>
2020-01-21 11:15:48 +0300

    modified init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,43 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+from config import config_options
+
+
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+bootstrap = Bootstrap()
 
-from app import routes
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    
+
+    # Registering the main app Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Registering auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = "/authenticate")
+    
+    # # Configure UploadSet
+    # configure_uploads(app, photos)
+
+    return app
+# from app import routes
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import app
-from app import create_app, db
+from app import create_app
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Post, Quotes
